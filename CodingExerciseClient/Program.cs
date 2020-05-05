58cd82b4e6b9768d0d6732f43c127ae45411ccc8
Michael Fuchs <michael.fuchs@inetfox.net>
2020-05-05 17:24:05 +0200

    initial version

---
+++ CodingExerciseClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Pde.CodingExercise.RandomNumberGenerator;
+using System;
+
+namespace CodingExerciseClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberGenerator numberGenerator = new NumberGenerator();
+
+            Console.ReadLine();
+            numberGenerator.NumberGenerated += NumberGenerator_NumberGenerated;
+
+            Console.ReadLine();
+        }
+
+        private static void NumberGenerator_NumberGenerated(object sender, NumberGeneratedEventArgs e)
+        {
+            Console.WriteLine($"Number generated {e.Number}");
+        }
+    }
+}
---
+++ RandomNumberGenerator/NumberGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+
+namespace Pde.CodingExercise.RandomNumberGenerator
+{
+    public class NumberGenerator
+    {
+        public event EventHandler<NumberGeneratedEventArgs> NumberGenerated;
+
+        Timer timer;
+        Random r = new Random();
+
+        public NumberGenerator()
+        {
+            timer = new Timer(500);
+            timer.Start();
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            OnNumberGenerated(new EventArgs());
+        }
+
+        protected virtual void OnNumberGenerated(EventArgs e)
+        {
+            EventHandler<NumberGeneratedEventArgs> handler = NumberGenerated;
+
+            NumberGeneratedEventArgs args = new NumberGeneratedEventArgs();
+            args.Number = r.Next(-1000, 1000);
+
+            handler?.Invoke(this, args);
+        }
+    }
+
+    public class NumberGeneratedEventArgs : EventArgs
+    {
+        public int Number { get; set; }
+    }
+}
